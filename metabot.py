bcd5991c383eba48f9f94d12f4d1b2c6bcc1fd73
Amelia <47516546+tainella@users.noreply.github.com>
2020-05-23 23:47:03 +0300

    Update metabot.py

--- metabot.py
+++ metabot.py
@@ -25,19 +25,36 @@ async def send(callback_query: types.CallbackQuery):
     if code.isdigit():
         code = int(code)
     if code == 1:
-        await bot.send_message(callback_query.from_user.id, f'Нажаты эмоции')
+    	await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id,text = "Эмоции", reply_markup = kb.inline_kb_emotion)
     elif code == 2:
-        await bot.send_message(callback_query.from_user.id, f'Нажаты разработки')
-
+    	await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id,text = "Темы для разговора", reply_markup = kb.inline_kb_back)
+    elif code == 3:
+    	await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id,text = "Описание", reply_markup = kb.inline_kb_des)
+    elif code == 4:
+    	await bot.delete_message(callback_query.message.chat.id, callback_query.message.message_id)
+    	ran = dbworker.random_mood('amusement')
+    	await bot.send_message(chat_id=callback_query.message.chat.id, text = "На фотографии изображен либо человек, либо ситуация, описывающая эмоции. Давай попробуем определить, какие")
+    	captio = 'Напиши свой вариант или сразу посмотри ответ'
+    	await bot.send_photo(chat_id=callback_query.message.chat.id,photo = ran,caption= captio, reply_markup = kb.inline_kb_train)
+    elif code == 5:
+    	await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id,text = "Привет :) я могу помочь тебе в общении с людьми. Разбираюсь в таких темах:", reply_markup = kb.inline_kb_start)
+    elif code == 6:
+    elif code == 7:
+    elif code == 8:
+    elif code == 9:
+    elif code == 10:
+    elif code == 11:
+    elif code == 12:
 @dp.message_handler(commands=["start"])
 async def cmd_start(message: types.Message):
-	await message.reply("Привет :) я могу помочь тебе в общении с людьми.<br> У меня есть информация по таким темам:", reply_markup=kb.inline_kb_start)
+	await bot.send_message(chat_id = message.chat.id,text = "Привет :) я могу помочь тебе в общении с людьми. Разбираюсь в таких темах:", reply_markup=kb.inline_kb_start)
 
 @dp.message_handler(commands=["random"])
 async def cmd_start(message: types.Message):
 	ran = dbworker.random_mood('amusement')
 	await bot.send_photo(message.chat.id, ran)
 
+#парсер датасета в базу данных
 @dp.message_handler(commands=["load"])
 async def load(message: types.Message):
 	#загрузка файлов на телеграм сервер -> добавление в базу данных

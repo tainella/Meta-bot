bdff9f6acde2dfca077a1300c02770f891e85d3e
Amelia <47516546+tainella@users.noreply.github.com>
2020-05-27 00:30:54 +0300

    Update dbworker.py

--- dbworker.py
+++ dbworker.py
@@ -1,7 +1,10 @@
 #база данных Media для file_id фото на сервере телеграма, загруженных из https://www.cs.rochester.edu/u/qyou/deepemotion/
 import config
 import os
+import datetime
+from datetime import timedelta
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, and_
+from sqlalchemy import func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine # для настроек
 from sqlalchemy.orm import relationship 
@@ -64,6 +67,7 @@ def photo_des(moo):
 #ТАБЛИЦА РАБОТЫ КАЖДОГО ПОЛЬЗОВАТЕЛЯ С РАЗДЕЛОМ ТРЕНИРОВКИ
 #на каждое не усвоенное фото пользователем фото строка с уточнением пользователя,
 #первым временем ознакомления, последним использованием, "правильный/неправильный" последний ответ
+count = 0 #индивидуальный ключ для добавления в dop
 class Train(Base):
     __tablename__ = 'Train'
     file_id = Column(String(255)) #файловое айди
@@ -71,26 +75,53 @@ class Train(Base):
     dop = Column(Integer, primary_key=True)
     date_in = Column(DateTime) #первое время ознакомления
     last_in = Column(DateTime) #последнее время использования
+    step = Column(Integer) #интервал повторения - "шаг выучивания"
     an = Column(Boolean()) #"правильный/неправильный" последний ответ
 
-#создать строку с новым файлом-юзер
-def add_one(chat, file, datein, ann):
+#шаги выучивания:
+#1 - 1 час; 2 - 24 часа; 3 - 48 часов; 4 - 72 часа
+#если хоть раз ответил на картинку неправильно, картинка добавляется в список повторения, 
+#выходит из него только при финальном правильном ответе через 72 часа
+	
+#изменить последнюю работу с файл-юзер (или добавить новую)
+def change_one(chat, file, ann):
 	session = Session()
 	if (None == session.query(Train).filter(and_(file_id=file, chat_id = chat))):
-		newitem = Train(file_id = file, chat_id = chat, date_in = datein, last_in = datein, an = ann)
+		count += 1
+		newitem = Train(file_id = file, chat_id = chat, dop = count, date_in = datetime.datetime.now(), last_in = datetime.datetime.now(), an = ann)
 		session.add(newitem)
-		session.commit()
-	session.close()
-	
-#изменить последнюю работу с файл-юзер
-def change_one(chat, file, last, ann):
-	session = Session()
-	if (None != session.query(Train).filter(and_(file_id=file, chat_id = chat))):
+	else:
 		vr = session.query(Train).filter(and_(file_id=file, chat_id = chat)).one()
-		newitem = Train(file_id = file, chat_id = chat, date_in = vr.datein, last_in = last, an = ann)
-		session.add(newitem)
-		session.commit()
+		if ann == True: #увеличение интервала повторения
+			if vr.step == 4:
+				session.delete(vr)
+			else:
+				vr.step += 1
+		else: #уменьшение интервала повторения
+			vr.step = 1
+		vr.last_in = datetime.datetime.now()
+		vr.an = ann
+	session.commit()
 	session.close()
+	session = Session()
+	vr = session.query(Train).filter(and_(file_id=file, chat_id = chat)).one()
+	print(vr)
+	session.close()
+
+#достать картинку для повторения по:
+# 1 приоритет: максимальная длина интервала (почти доученные фото)
+# 2 приоритет: дата первого ознакомления
+def repeat(chat):
+	session = Session()
+	vr = session.query(Train).filter(chat_id = chat, func.max(step), (last_in < (datetime.datetime.now() - timedelta(minutes=55)))).one()
+	if vr.step < 48:
+		h = session.query(Train).filter(chat_id = chat, step = vr.step, func.min(date_in)).one()
+	else:
+		h = vr
+	return h.file_id
+
+#достать новую картинку для изучения - def random_mood(mood) (описана выше)
+
 	
 #БАЗА ДАННЫХ ПОЛЬЗОВАТЕЛЕЙ
 class Us(Base):
